d07259c8461c5b5e3511d1840550e572f2028aec
Fatih <fatihdumanli@live.com>
2017-07-30 02:22:53 +0300

    Add project files.

---
+++ AsyncContentLoading/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsyncContentLoading.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+    
+        public ActionResult SyncPage()
+        {
+            var images = new List<string>()
+            {
+                "http://fatihdumanli.com/fatih/bosphorus/1.jpg",
+                "http://fatihdumanli.com/fatih/bosphorus/1.jpg",
+                "http://fatihdumanli.com/fatih/bosphorus/3.jpg",
+                "http://fatihdumanli.com/fatih/bosphorus/4.jpg",
+            };
+
+            return View(images);
+        }
+
+
+
+
+
+
+
+        public ActionResult GetImageData()
+        {
+            var images = new List<string>()
+            {
+                "http://fatihdumanli.com/fatih/bosphorus/1.jpg",
+                "http://fatihdumanli.com/fatih/bosphorus/1.jpg",
+                "http://fatihdumanli.com/fatih/bosphorus/3.jpg",
+                "http://fatihdumanli.com/fatih/bosphorus/4.jpg",
+            };
+
+            return Json(images, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+
+
+        public ActionResult AsyncPage()
+        {  
+            return View();
+        }
+
+
+
+
+
+    }
+}
